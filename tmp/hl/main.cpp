a5f1fb15b8ca72d4d37bd9bc066607d34bfe43fa
VaL <valbok@gmail.com>
2015-05-19 12:06:02 +0300

    45

--- tmp/hl/main.cpp
+++ tmp/hl/main.cpp
@@ -5,14 +5,7 @@
 
 using namespace std;
 
-/** 
- * Simplify the implementation below as much as you can. 
- * Even better if you can also improve performance as part of the simplification!
- * FYI: This code is over 35 lines and over 300 tokens, but it can be written in
- * 5 lines and in less than 60 tokens.
-*/
-
-char *func(char *s, char a, char b) 
+char *find(char *s, char a, char b) 
 {
     char *res = NULL;
     int i = 0;
@@ -31,38 +24,44 @@ int main()
 {
     {
         char *s = "test string";
-        char *r = func(s, 'e', 's');
+        char *r = find(s, 'e', 's');
         assert(strcmp(r, "est string") == 0);
     }
     
     {
         char *s = "test string";
-        char *r = func(s, 'r', 's');
+        char *r = find(s, 'r', 's');
         assert(strcmp(r, "st string") == 0);
     }
 
     {
         char *s = "test string";
-        char *r = func(s, 'e', 's');
+        char *r = find(s, 'e', 's');
         assert(strcmp(r, "est string") == 0);
+    }
+
+    {
+        char *s = "test string";
+        char *r = find(s, 'p', 'q');
+        assert(r == NULL);
     }
     
     {
         char s[1] = {'\0'};
-        char *r = func(s, 'r', 's');
+        char *r = find(s, 'r', 's');
         assert(r == NULL);
     }
 
     {
         char s[1] = {'\0'};
-        char *r = func(s, 'r', '\0');
+        char *r = find(s, 'r', '\0');
         assert(strcmp(r, "\0") == 0);
         assert(*r == '\0');
     }
 
     {
         char s[1] = {'\0'};
-        char *r = func(s, '\0', 't');
+        char *r = find(s, '\0', 't');
         assert(strcmp(r, "\0") == 0);
         assert(*r == '\0');
     }
