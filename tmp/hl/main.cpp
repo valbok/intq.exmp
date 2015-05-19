629be453364e8b87656448f4c748051751fb9792
VaL <valbok@gmail.com>
2015-05-19 11:54:07 +0300

    45

--- tmp/hl/main.cpp
+++ tmp/hl/main.cpp
@@ -6,12 +6,10 @@
 using namespace std;
 
 /** 
-Problem
-===========
-Simplify the implementation below as much as you can. 
-Even better if you can also improve performance as part of the simplification!
-FYI: This code is over 35 lines and over 300 tokens, but it can be written in
-5 lines and in less than 60 tokens.
+ * Simplify the implementation below as much as you can. 
+ * Even better if you can also improve performance as part of the simplification!
+ * FYI: This code is over 35 lines and over 300 tokens, but it can be written in
+ * 5 lines and in less than 60 tokens.
 */
 
 char *func(char *s, char a, char b) 
