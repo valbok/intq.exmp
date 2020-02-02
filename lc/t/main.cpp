867e716aeb2dcf09b7e13ebf98fccadd241e281a
VaL Doroshchuk <valbok@gmail.com>
2020-02-02 12:32:59 +0100

    f

--- lc/t/main.cpp
+++ lc/t/main.cpp
@@ -1,5 +1,22 @@
 Given a binary tree, find its maximum depth.
 
+int maxDepth(Node * root)
+{
+  // Root being null means tree doesn't exist.
+  if (root == NULL)
+    return 0;
+  
+  // Get the depth of the left and right subtree 
+  // using recursion.
+  int leftDepth = maxDepth(root->left);
+  int rightDepth = maxDepth(root->right);
+
+  // Choose the larger one and add the root to it.
+  if (leftDepth > rightDepth)
+    return leftDepth + 1;
+  else
+    return rightDepth + 1;
+}
 ***
 Given a binary tree, determine if it is a valid binary search tree (BST).
 
