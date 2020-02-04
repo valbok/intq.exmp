46ed598c2cda1da1c0a1a712e2dff94f597ceadc
VaL Doroshchuk <valbok@gmail.com>
2020-02-04 17:25:55 +0100

    f

--- lc/s/main.cpp
+++ lc/s/main.cpp
@@ -1,4 +1,24 @@
+Input:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
 
+Output: [1,2,2,3,5,6]
+
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        if (m == 0) {
+            nums1 = nums2;
+            return;
+        }
+        int i = m - 1, j = n - 1, k = nums1.size() - 1;
+        while (k>=0) {
+            if (i>=0 && j>=0 && nums1[i] > nums2[j]){
+                nums1[k--] = nums1[i--];
+            } else if (j>=0)  {
+                nums1[k--] = nums2[j--];
+            } else break;
+        }
+
+    }
 ****
 
 You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
@@ -11,9 +31,9 @@ int firstBadVersion(int n) {
             if (isBadVersion(m)) {
                 e = m;
             } else {
-                b = m + 1;                
+                b = m + 1;
             }
         }
-        
+
         return e;
     }
--- lc/t/main.cpp
+++ lc/t/main.cpp
@@ -5,8 +5,8 @@ int maxDepth(Node * root)
   // Root being null means tree doesn't exist.
   if (root == NULL)
     return 0;
-  
-  // Get the depth of the left and right subtree 
+
+  // Get the depth of the left and right subtree
   // using recursion.
   int leftDepth = maxDepth(root->left);
   int rightDepth = maxDepth(root->right);
@@ -20,28 +20,77 @@ int maxDepth(Node * root)
 ***
 Given a binary tree, determine if it is a valid binary search tree (BST).
 
+ bool rec(TreeNode* root, long &min, long &max) {
+        if (!root)return true;
+        if (root->val < min)
+            min = root->val;
+        if (root->val > max)
+            max = root->val;
+
+        long mmin = LONG_MAX, mmax = LONG_MIN;
+        if (!rec(root->left, mmin, mmax)) return false;
+        if (root->val <= mmax) return false;
+        if (mmin < min) min = mmin;
+        if (mmax > max) max = mmax;
+
+        mmin = LONG_MAX;
+        if (!rec(root->right,mmin, mmax)) return false;
+        if (root->val >= mmin) return false;
+        if (mmin < min) min = mmin;
+        if (mmax > max) max = mmax;
+        return true;
+    }
+
+    bool isValidBST(TreeNode* root) {
+        long min = LONG_MAX, max = LONG_MIN;
+        return rec(root, min, max);
+    }
+
 ***
 Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+bool isSymmetric(TreeNode* root) {
+        if (!root) return true;
+        stack<TreeNode*>s;
+        s.push(root->left);
+        s.push(root->right);
+        while (s.size()) {
+            auto right = s.top();s.pop();
+            auto left = s.top();s.pop();
+            if (!left && right) return false;
+            if (left && !right) return false;
+            if (!left && !right) continue;
+            if (left->val != right->val) return false;
+
+            s.push(left->left);
+            s.push(right->right);
+            s.push(left->right);
+            s.push(right->left);
+        }
+
+        return true;
+
+    }
 ***
 Given a binary tree, return the level order traversal of its nodes values. (ie, from left to right, level by level).
 
 vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>> r;
-        if (!root) return r;        
+        if (!root) return r;
         queue<TreeNode*> q;
         q.push(root);
         vector<int> v;
         int n = 1, i = 0, inserted = 0;
-        while (!q.empty()) {            
-            auto node = q.front();q.pop();            
+        while (!q.empty()) {
+            auto node = q.front();q.pop();
             v.push_back(node->val);
-           
+
             if (node->left) {
                 q.push(node->left); ++inserted;
             }
             if (node->right) {
                 q.push(node->right); ++inserted;
-            }          
+            }
             if (++i == n) {
                 r.push_back(v);
                 v.clear();
@@ -59,10 +108,10 @@ Given an array where elements are sorted in ascending order, convert it to a hei
         if (nums.empty()) return nullptr;
         int n = nums.size()/2;
         auto node = new TreeNode(nums[n]);
-        vector<int> ll(nums.begin(),nums.begin()+n);        
+        vector<int> ll(nums.begin(),nums.begin()+n);
         node->left = sortedArrayToBST(ll);
 
-        vector<int> rr(nums.begin()+n+1,nums.end());        
+        vector<int> rr(nums.begin()+n+1,nums.end());
         node->right = sortedArrayToBST(rr);
 
         return node;
