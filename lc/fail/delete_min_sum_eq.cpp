ec2e593c9dd0578aa41386d12192faf5f991e64c
VaL Doroshchuk <valentyn.doroshchuk@qt.io>
2020-02-04 16:37:26 +0100

    f

---
+++ lc/fail/delete_min_sum_eq.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+
+int solution(vector<int> &A, int r) {
+
+    return 0;
+}
+int main() {
+    vector<int>A={10,20,30,60};
+    cout <<solution(A,2)<<endl;
+
+}
