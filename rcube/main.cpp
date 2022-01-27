c172ced05790b2d38bef87c6fe958e14a8156d15
VaL Doroshchuk <valentyn.doroshchuk@qt.io>
2022-01-27 22:09:03 +0100

    tmp

--- rcube/main.cpp
+++ rcube/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 #include <queue>
+#include <stack>
 #include <map>
 #include <algorithm>
 
@@ -12,7 +13,7 @@ static void print_path(State st, const State &s, const std::map<State, std::pair
         if (it == parent.end())
             break;
         st = it->second.first;
-        std::cout << "  " << ip << "> step:" << it->second.second << std::endl;
+        std::cout << "  " << ip << "> step: " << State::step_to_str(it->second.second) << std::endl;
         st.print();
     }
 }
@@ -29,42 +30,60 @@ int main()
     s.step(State::LEFT2);
     s.print();*/
 
-    s._state[State::YELLOW][2][0] = State::ORANGE;
-    s._state[State::YELLOW][2][1] = State::WHITE;
-    s._state[State::YELLOW][2][2] = State::WHITE;
+    /*s._state[State::YELLOW][2][1] = State::WHITE;
 
-    s._state[State::BLUE][0][0] = State::WHITE;
 
-    s._state[State::WHITE][0][1] = State::YELLOW;
-    s._state[State::WHITE][0][2] = State::YELLOW;
-    s._state[State::WHITE][2][0] = State::GREEN;
+    s._state[State::WHITE][0][1] = State::ORANGE;
 
-    s._state[State::GREEN][0][2] = State::ORANGE;
-    s._state[State::GREEN][2][0] = State::RED;
 
-    s._state[State::RED][2][0] = State::BLUE;
-    s._state[State::RED][2][2] = State::YELLOW;
+    s._state[State::ORANGE][2][1] = State::YELLOW;
+*/
+s._state[State::YELLOW][2][1] = State::GREEN;
 
-    s._state[State::ORANGE][2][0] = State::GREEN;
-    s._state[State::ORANGE][2][2] = State::RED;
+
+    s._state[State::WHITE][0][0] = State::RED;
+    s._state[State::WHITE][0][1] = State::RED;
+    s._state[State::WHITE][0][2] = State::ORANGE;
+    s._state[State::WHITE][1][0] = State::GREEN;
+    s._state[State::WHITE][1][2] = State::GREEN;
+
+    s._state[State::GREEN][0][1] = State::YELLOW;
+    s._state[State::GREEN][1][0] = State::ORANGE;
+    s._state[State::GREEN][1][2] = State::WHITE;
+    s._state[State::GREEN][2][0] = State::WHITE;
+    s._state[State::GREEN][2][1] = State::WHITE;
+    s._state[State::GREEN][2][2] = State::WHITE;
+
+    s._state[State::RED][2][0] = State::GREEN;
+    s._state[State::RED][2][1] = State::GREEN;
+
+    s._state[State::ORANGE][1][2] = State::WHITE;
+    s._state[State::ORANGE][2][2] = State::GREEN;
 
     if (!s.validate())
         return 1;
 
     s.print();
 
-    std::queue<State> q;
+    std::cout <<"0:"<<s.entropy(0)<<std::endl;
+    std::cout <<"1:"<<s.entropy(1)<<std::endl;
+    std::cout <<"2:"<<s.entropy(2)<<std::endl;
+    //return 0;
+    //std::queue<State> q;
+    //std::queue<std::pair<State,int>> q;
+    std::stack<std::pair<State,int>> q;
     std::map<State, std::pair<State, State::Step>> parent;
 
     
-        q.push(s);
+        //q.push(s);
+    q.push(std::make_pair(s, 0));
 
     int steps_count = 0;
     //std::vector<State::Step> steps = {State::UP2, State::DOWN2, State::LEFT2, State::RIGHT2, State::UP5, State::DOWN5};
     //std::vector<State::Step> steps = {State::LEFT2, State::RIGHT2, State::UP5, State::DOWN5};
     
 
-std::vector<State::Step> steps = {
+    std::vector<State::Step> steps = {
         State::UP0, State::DOWN0, State::LEFT0, State::RIGHT0,
         State::UP1, State::DOWN1, State::LEFT1, State::RIGHT1,
         State::UP2, State::DOWN2, State::LEFT2, State::RIGHT2,
@@ -75,10 +94,13 @@ std::vector<State::Step> steps = {
     //steps = {State::UP2,  State::LEFT2, State::UP5};
 
     int prevEntropy = s.entropy();
-    while (!q.empty()) {
+    std::vector<int> vc = {0};
+int ce = 0;
+    /*while (!q.empty()) {
         
         auto st = q.front();
-        std::cout << "[" << steps_count++ << "] "<<q.size() << " e:" << prevEntropy << "->" << st.entropy() << "                 \r";
+        //std::cout << "[" << steps_count++ << "] "<<q.size() << " e:" << prevEntropy << "->" << st.entropy() << "                 \r";
+        std::cout << "[" << st.second << "/" << vc[st.second] << "] "<<q.size() << " e:" << prevEntropy << "->" << st.first.entropy() << "                 \r";//<< std::endl;
         q.pop();
 
         //st.print();
@@ -86,17 +108,14 @@ std::vector<State::Step> steps = {
         //std::vector<State> pending;
         for (int i = 0; i < steps.size(); ++i) {
             auto step = steps[i];
-            auto cp = st;
+            //auto cp = st;
+            auto cp = st.first;
             cp.step(step);
+cp.print();
 
             auto it = parent.find(cp);
             if (it == parent.end()) {
-                //std::cout << step<<":"<<st.entropy() << "-" << cp.entropy()<<"="<<(cp.entropy()-st.entropy())<<std::endl;
-                //std::cout<<"--------\n";
-                //cp.print();
-                
-                //if (cp.entropy() < st.entropy()) {
-                parent[cp] = std::make_pair(st, step);
+                parent[cp] = std::make_pair(st.first, step);
                 if (cp.entropy() < prevEntropy) {
                     std::cout << "\n:::::::::" << prevEntropy << "-------------->" << cp.entropy()<<std::endl;
                     prevEntropy = cp.entropy();
@@ -108,37 +127,111 @@ std::vector<State::Step> steps = {
 
                 }
 
-                if (cp.entropy() - prevEntropy <= 15)
-                q.push(cp);
+                //if (cp.entropy() - prevEntropy <= 20)
+                //if (cp.entropy(ce) == 0)                    ++ce;
+                
+                if (cp.entropy() <= 24) {
+                //q.push(cp);
+                    q.push(std::make_pair(cp, st.second+1));
 
-                //parent[cp] = std::make_pair(st, step);
-                //}
-
+                    if (vc.size() >= st.second + 1)
+                        vc.push_back(0);
+                    ++vc[st.second + 1];
+                }
 
                 if (cp == solved || cp.entropy() == 0) {
                     std::cout << steps_count << "============================================\n";
 
                     cp.print();
                     print_path(cp, s, parent);
-                    /*for (int ip = 0; cp != s; ++ip) {
-                        auto it = parent.find(cp);
-                        if (it == parent.end())
-                            break;
-                        cp = it->second.first;
-                        std::cout << ip << ":------" << it->second.second << std::endl;
-                        cp.print();
-                    }*/
 
                     return 0;
                 }  
             }
 
         }
-        //if (q.size() < 100000000) {
-        //std::sort(pending.begin(), pending.end(), [] (State &a, State &b) { return a.entropy() < b.entropy(); });
-        //for (auto &a:pending)            q.push(a);
-        //}
+    }*/
 
+    while (!q.empty()) {
+        
+        auto st = q.top();
+        //std::cout << "[" << steps_count++ << "] "<<q.size() << " e:" << prevEntropy << "->" << st.entropy() << "                 \r";
+        std::cout << "[" << st.second << "/" << vc[st.second] << "] "<<q.size() << " e:" << prevEntropy << "->" << st.first.entropy() << "                 \r";//<< std::endl;
+        q.pop();
+
+        //st.print();
+
+        //std::vector<State> pending;
+        for (int i = 0; i < steps.size(); ++i) {
+            auto step = steps[i];
+            //auto cp = st;
+            auto cp = st.first;
+            cp.step(step);
+cp.print();
+
+            auto it = parent.find(cp);
+            if (it == parent.end()) {
+                //std::cout << step<<":"<<st.entropy() << "-" << cp.entropy()<<"="<<(cp.entropy()-st.entropy())<<std::endl;
+                //std::cout<<"--------\n";
+                //cp.print();
+                
+                //if (cp.entropy() < st.entropy()) {
+                //parent[cp] = std::make_pair(st, step);
+                parent[cp] = std::make_pair(st.first, step);
+                /*if (cp.entropy() < prevEntropy) {
+                    std::cout << "\n:::::::::" << prevEntropy << "-------------->" << cp.entropy()<<std::endl;
+                    prevEntropy = cp.entropy();
+                    cp.print();
+                    //parent[cp] = std::make_pair(st, step);
+                    print_path(cp, s, parent);
+                    //parent.clear();
+                    //q = std::queue<State>(); 
+
+                }*/
+
+                //if (cp.entropy() - prevEntropy <= 20)
+                //if (cp.entropy(ce) == 0)                    ++ce;
+                
+                for (int iii = 0; iii < 3; ++iii) {
+                    std::cout <<"  " << iii <<":"<<cp.entropy(iii)<<"vs"<<s.entropy(iii)<<std::endl;
+                    if (cp.entropy(iii) == 0) continue;
+
+                    if (cp.entropy(iii) > s.entropy(iii))
+                        break;
+
+
+std::cout <<"++++++::"<<cp.entropy() <<"vs"<<s.entropy()<<std::endl<<std::endl;
+
+                    q.push(std::make_pair(cp, st.second+1));
+
+                    if (vc.size() >= st.second + 1)
+                        vc.push_back(0);
+                    ++vc[st.second + 1];
+                    break;
+                }
+
+                /*if (cp.entropy() <= 24) {
+                //q.push(cp);
+                    q.push(std::make_pair(cp, st.second+1));
+
+                    if (vc.size() >= st.second + 1)
+                        vc.push_back(0);
+                    ++vc[st.second + 1];
+                }*/
+
+                if (cp == solved || cp.entropy() == 0
+|| cp.entropy(1) == 0
+                    ) {
+                    std::cout << steps_count << "============================================\n";
+
+                    cp.print();
+                    print_path(cp, s, parent);
+
+                    return 0;
+                }  
+            }
+
+        }
     }
 
     return 0;
--- rcube/state.h
+++ rcube/state.h
@@ -35,23 +35,50 @@ public:
         RIGHT1,
 
         UP2, // 8
-        DOWN2,
+        DOWN2, // 9
 
         LEFT2, // 10
         RIGHT2, //11
 
-        UP3,
-        DOWN3,
+        UP3, //12
+        DOWN3, //13
 
-        UP4,
-        DOWN4,
+        UP4, // 14
+        DOWN4,// 15
 
-        UP5,
-        DOWN5,
+        UP5, // 16
+        DOWN5, // 17
 
         END
 
     };
+
+    static const char *step_to_str(Step s)
+    {
+        switch (s) {
+            case Step::UP0: return "UP0";
+            case Step::DOWN0: return "DOWN0";
+            case Step::LEFT0: return "LEFT0";
+            case Step::RIGHT0: return "RIGHT0";
+            case Step::UP1: return "UP1";
+            case Step::DOWN1: return "DOWN1";
+            case Step::LEFT1: return "LEFT1";
+            case Step::RIGHT1: return "RIGHT1";
+            case Step::UP2: return "UP2";
+            case Step::DOWN2: return "DOWN2";
+            case Step::LEFT2: return "LEFT2";
+            case Step::RIGHT2: return "RIGHT2";
+            case Step::UP3: return "UP3";
+            case Step::DOWN3: return "DOWN3";
+            case Step::UP4: return "UP4";
+            case Step::DOWN4: return "DOWN4";
+            case Step::UP5: return "UP5";
+            case Step::DOWN5: return "DOWN5";
+            default:
+                break;
+        }
+        return "";
+    }
 
     constexpr static const int YELLOW = 0;
     constexpr static const int BLUE = 1;
@@ -406,6 +433,56 @@ public:
                         ++e;
                 }
             }
+        }
+
+        return e;
+    }
+
+    int entropy(int k) const
+    {
+        int e = 0;
+        switch (k) {
+            case 0:
+                e += _state[YELLOW][0][0] != YELLOW || _state[BLUE][2][0] != BLUE || _state[RED][0][2] != RED;
+                e += _state[YELLOW][0][1] != YELLOW || _state[BLUE][2][1] != BLUE;
+                e += _state[YELLOW][0][2] != YELLOW || _state[BLUE][2][2] != BLUE || _state[ORANGE][0][0] != ORANGE;
+
+                e += _state[BLUE][1][0] != BLUE || _state[RED][0][1] != RED;
+                e += _state[BLUE][1][1] != BLUE;
+                e += _state[BLUE][1][2] != BLUE || _state[ORANGE][0][1] != ORANGE;
+
+                e += _state[BLUE][0][0] != BLUE || _state[WHITE][2][0] != WHITE || _state[RED][0][0] != RED;
+                e += _state[BLUE][0][1] != BLUE || _state[WHITE][2][1] != WHITE;
+                e += _state[BLUE][0][2] != BLUE || _state[WHITE][2][2] != WHITE || _state[ORANGE][0][2] != ORANGE;
+                break;
+            case 1:
+                e += _state[YELLOW][1][0] != YELLOW || _state[RED][1][2] != RED;
+                e += _state[YELLOW][1][1] != YELLOW;
+                e += _state[YELLOW][1][2] != YELLOW || _state[ORANGE][1][0] != ORANGE;
+
+                e += _state[RED][1][1] != RED;
+                e += _state[RED][1][0] != RED || _state[WHITE][1][0] != WHITE;
+
+                e += _state[ORANGE][1][1] != ORANGE;
+                e += _state[ORANGE][1][2] != ORANGE || _state[WHITE][1][2] != WHITE;
+
+                e += _state[WHITE][1][1] != WHITE;
+                break;
+            case 2:
+                e += _state[YELLOW][2][0] != YELLOW || _state[GREEN][0][0] != GREEN || _state[RED][2][2] != RED;
+                e += _state[YELLOW][2][1] != YELLOW || _state[GREEN][0][1] != GREEN;
+                e += _state[YELLOW][2][2] != YELLOW || _state[GREEN][0][2] != GREEN || _state[ORANGE][2][0] != ORANGE;
+
+                e += _state[GREEN][1][0] != GREEN || _state[RED][2][1] != RED;
+                e += _state[GREEN][1][1] != GREEN;
+                e += _state[GREEN][1][2] != GREEN || _state[ORANGE][2][1] != ORANGE;
+
+                e += _state[GREEN][2][0] != GREEN || _state[WHITE][0][0] != WHITE || _state[RED][2][0] != RED;
+                e += _state[GREEN][2][1] != GREEN || _state[WHITE][0][1] != WHITE;
+                e += _state[GREEN][2][2] != GREEN || _state[WHITE][0][2] != WHITE || _state[ORANGE][2][2] != ORANGE;
+                break;
+            default:
+                break;
         }
 
         return e;
